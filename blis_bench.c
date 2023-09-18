0650d38761343ce3e05deb9bfbaf665b16d8c468
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-09-18 15:35:33 -0700

    add blis_bench.c

---
+++ blis_bench.c
@@ -0,0 +1,46 @@
+#include <stdint.h>  // for uint8_t
+#include <stdio.h>   // for printf, fprintf, FILE
+#include <stdlib.h>  // for free, malloc, posix_memalign
+#include <time.h>    // for clock_gettime, timespec, CLOCK_MONOTONIC
+
+#include "./Bench.h"
+#include "./Matmul.h"
+
+int main(void) {
+  uint8_t *restrict A;
+  posix_memalign((void **)&A, 128, M * K * sizeof(uint8_t));
+  for (int i = 0; i < M * K; ++i) {
+    A[i] = (uint8_t)arc4random();
+  }
+
+  uint8_t *restrict B;
+  posix_memalign((void **)&B, 128, K * N * sizeof(uint8_t));
+  for (int i = 0; i < K * N; ++i) {
+    B[i] = (uint8_t)arc4random();
+  }
+
+  uint8_t *restrict C;
+  posix_memalign((void **)&C, 128, M * N * sizeof(uint8_t));
+  for (int i = 0; i < M * N; ++i) {
+    C[i] = 0;
+  }
+
+  // Inlined kernel follows. This is for warm-up.
+  Blis(A, B, C, 32, 128, 16);
+
+  struct timespec start, end;
+#pragma clang loop unroll(disable)
+  for (int i = 0; i < 10; ++i) {
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    Blis(A, B, C, 32, 128, 16);
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    printf("%lds %ldns\n", TsDiff(start, end).tv_sec,
+           TsDiff(start, end).tv_nsec);
+  }
+
+  free(A);
+  free(B);
+  free(C);
+
+  return 0;
+}
