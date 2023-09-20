38654b53cfa850c03a3badc99a5f322997b34eba
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-09-19 21:12:38 -0700

    Fix to make all matrices row-major to conform to formal math

--- Naive.c
+++ Naive.c
@@ -1,10 +1,10 @@
 #include "Naive.h"
 
 void Naive(uint8_t *restrict A, uint8_t *restrict B, uint8_t *restrict C) {
-  for (int j = 0; j < N; ++j) {
-    for (int p = 0; p < K; ++p) {
-      for (int i = 0; i < M; ++i) {
-        C[i * N + j] += A[p * N + j] * B[p * N + i];
+  for (int m = 0; m < M; ++m) {
+    for (int n = 0; n < N; ++n) {
+      for (int k = 0; k < K; ++k) {
+        C[m * N + n] += A[m * K + k] * B[k * N + n];
       }
     }
   }
